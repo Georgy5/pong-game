4162fabb9db1936089fabc995f549f5218c8bcf7
Karl Keller <karl.keller@protonmail.com>
2023-11-24 15:57:20 +0100

    Ball bounces off left paddle

--- pong.js
+++ pong.js
@@ -10,16 +10,27 @@ const RADIUS = 10
 
 // render rectangle
 function renderField() {
-  ctx.fillStyle = 'black';
-  ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 }
+
+const paddleWidth = 15;
+const paddleLength = 80;
+const leftPaddleX = 20;
 
 // render left paddle
 function renderPaddle(y) {
   ctx.fillStyle = 'white';
-  ctx.fillRect(20, y, 15, 80);
+  ctx.fillRect(leftPaddleX, y, paddleWidth, paddleLength);
 }
 
+// debug ball
+function renderWhiteBall() {
+  ctx.beginPath();
+  ctx.arc(lPaddleWallX, 70, 5, 0, 2 * Math.PI);
+  ctx.fillStyle = 'white';
+  ctx.fill();
+}
 
 // render circles
 function renderBall(x, y) {
@@ -29,40 +40,65 @@ function renderBall(x, y) {
   ctx.fill();
 }
 
+// find collisions
+const collision = () => {
+    canvasWidth || canvasHeight 
+    // x + paddleWidth, y
+
+}
+// lPaddle() {}
+// 20 + 15 + 10 = 45 
+const lPaddleWallX = leftPaddleX + paddleWidth + RADIUS;
+
+// if paddleWall 
+console.log(lPaddleWallX);
+
 // Render Pong Game
 let posX = 50
 let posY = 50
 let vX = +2
 let vY = -2
-let paddleY = 20
+let leftPaddleY = 20
 setInterval(() => {
   renderField()
+
   renderBall(posX, posY)
-//   renderPaddle(paddleY)
+  renderPaddle(leftPaddleY)
 
   posX += vX
-  posY += vY
-
+//   posY += vY
+  collisionShouter(posX)
   if (posX + RADIUS === canvasWidth || posX - RADIUS === 0) {
+  vX = -1 * vX
+
+  } else if (posX + RADIUS === canvasWidth || posX === 44 && (posY >= leftPaddleY && posY <= leftPaddleY + paddleLengthss) ) {
     vX = -1 * vX
   }
-  if (posY + RADIUS === canvasHeight || posY - RADIUS === 0) {
-    vY = -1 * vY
-  }
+
+//   if (posY + RADIUS === canvasHeight || posY - RADIUS === 0) {
+//     vY = -1 * vY
+//   }
   
 
   // 3. Impact of the ball with the paddle in the X axis
 }, 17)
 
+const collisionShouter = (posX) => {
+  if (posX === 44) {
+    console.log("Collision with left paddle detected!");
+  };
+}
+
+
 document.addEventListener('keydown', (event) => {
   const key = event.key
   // const {key} = event
   if (key === 'w') {
-    if (paddleY >= 5) {
-      paddleY -= 5
+    if (leftPaddleY >= 5) {
+      leftPaddleY -= 5
     }
   }
   if (key === 's') {
-    paddleY += 5
+    leftPaddleY += 5
   }
 })
